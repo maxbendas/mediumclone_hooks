ff0068faa429a3bfd12351df5347f40e7e595458
maxbendas <maxbendas86@gmail.com>
2020-12-02 16:19:41 +0200

    6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'routes';
+import TopBar from 'components/topBar';
 import {BrowserRouter as Router} from "react-router-dom";
 
 const App = () => {
     return (
         <div>
-<h3>Welcome to hooks</h3>
             <Router>
+                <TopBar/>
                 <Routes/>
             </Router>
         </div>

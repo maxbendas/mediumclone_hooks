502ffb0c9891a75d8aaae70681ab73b6f2fa56d2
maxbendas <56072735+maxbendas@users.noreply.github.com>
2020-12-06 23:14:40 +0200

    19

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import Routes from 'routes';
 import TopBar from 'components/topBar';
 import {BrowserRouter as Router} from "react-router-dom";
+import{CurrentUserProvider} from 'contexts/currentUser'
 
 const App = () => {
     return (
-        <div>
+        <CurrentUserProvider>
             <Router>
                 <TopBar/>
                 <Routes/>
             </Router>
-        </div>
+        </CurrentUserProvider>
     )
 }
 
